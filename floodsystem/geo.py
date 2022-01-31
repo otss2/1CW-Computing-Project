b8c904d6052f36478105e54628b1eec2259638a9
Ben Atchison <bca33@cam.ac.uk>
2022-01-31 00:46:03 +0000

    Work done for Task 1B and 1C

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,35 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+from .station import MonitoringStation
+from haversine import haversine
+
+
+# Task 1B
+def stations_by_distance(stations,p):
+    station_name_distance = {}
+    for i in stations:
+        distance = haversine(stations.coord,p)
+        station_name_distance.append(stations.station_id, distance)
+   
+    sorted_by_key(station_name_distance,1)
+    return station_name_distance
+
+# Task 1C
+def stations_within_radius(stations, centre, radius):
+    x = centre
+    stations_in_radius = []
+    
+    for i in stations:
+        distance = haversine(stations.coord, x)
+        if distance < radius:
+            stations_in_radius.append(stations.station_id)
+        if distance == radius:
+            stations_in_radius.append(stations.station_id)
+        if distance > radius:
+            continue
+    return stations_in_radius
+
+
+
+
