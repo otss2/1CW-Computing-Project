d88d9a30dda80c51f062715446b1195d0ea66ade
Ben Atchison <bca33@cam.ac.uk>
2022-01-31 00:51:52 +0000

    1B almost done (can't import haversine?)

--- Task1B.py
+++ Task1B.py
@@ -1,6 +1,26 @@
+from floodsystem import geo
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import stations_by_distance
+
 def run():
-    print("HI")
+    """Requirements for Task 1B"""
+    
+    # Build list of stations
+    stations = build_station_list()
+
+    # Build list of stations and distances from coordinate
+    station_name_distance = geo.stations_by_distance(stations, (52.2053,0.1218))
+
+    print(station_name_distance)
+
+
+
+
 
 if __name__ == "__main__":
     print("*** Task 1B: CUED Part IA Flood Warning System ***")
     run()
+
+
+geo.stations_by_distance(build_station_list(), (52.2053,0.1218))
+print(stations_by_distance(build_station_list(),(52.2053,0.1218)))
