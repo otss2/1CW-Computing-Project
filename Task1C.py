f8d3b240879e3610e0efd4c52d34108f4d339046
Ben Atchison <bca33@cam.ac.uk>
2022-01-31 00:55:59 +0000

    1C almost done (need to check over + fix haversine)

--- Task1C.py
+++ Task1C.py
@@ -1,5 +1,18 @@
+from floodsystem import geo
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import stations_within_radius
+
 def run():
-    print("HI")
+    """Requirements for Task 1C"""
+
+    # Build list of stations
+    stations = build_station_list()
+
+    # Build list of stations within certain radius of specified point
+    stations_in_radius = stations_within_radius(stations, (52.2053, 0.1218), 10000)
+
+    print(stations_in_radius)
+
 
 if __name__ == "__main__":
     print("*** Task 1C: CUED Part IA Flood Warning System ***")
